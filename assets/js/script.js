d27c667502db2eebf57f05be47065b17d421d3f1
brian-moretti <brianmoretti2512@gmail.com>
2023-01-05 19:28:35 +0100

    Create a copy of main and added prettier format

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,66 +3,66 @@
 const intro = document.getElementById("intro");
 
 let introPhrase = document.createElement("h2");
-    introPhrase.innerHTML = "\"Reading is that island where all dreamers land.\"";
-    intro.append(introPhrase);
+introPhrase.innerHTML = '"Reading is that island where all dreamers land."';
+intro.append(introPhrase);
 
 let btnIntro = document.createElement("button");
-    btnIntro.setAttribute("id", "btnIntro");
-    btnIntro.setAttribute("type", "submit");
-    intro.append(btnIntro);
+btnIntro.setAttribute("id", "btnIntro");
+btnIntro.setAttribute("type", "submit");
+intro.append(btnIntro);
 
 let iconBtn = document.createElement("img");
-    iconBtn.setAttribute("src", "/assets/images/open-book.png");
-    iconBtn.setAttribute("width", "50px");
-    btnIntro.append(iconBtn);
+iconBtn.setAttribute("src", "/assets/images/open-book.png");
+iconBtn.setAttribute("width", "50px");
+btnIntro.append(iconBtn);
 
 btnIntro.addEventListener("click", () => {
-    intro.style.display = "none";
-    searchBox.style.display = "flex";
+  intro.style.display = "none";
+  searchBox.style.display = "flex";
 });
 
-// CODE --> TEXTBOX PAGE // 
+// CODE --> TEXTBOX PAGE //
 
 const searchBox = document.getElementById("searchBox");
 
 let searchPhrase = document.createElement("h2");
-    searchPhrase.innerHTML = "Choose Wisely...";
-    searchBox.append(searchPhrase);
+searchPhrase.innerHTML = "Choose Wisely...";
+searchBox.append(searchPhrase);
 
 let form = document.createElement("form");
-    form.setAttribute("name", "box");
-    form.setAttribute("onsubmit", "showBooks(); return false");
-
+form.setAttribute("name", "box");
+form.setAttribute("onsubmit", "showBooks(); return false");
 
 let labelInput = document.createElement("label");
-    labelInput.setAttribute("id","labelInput");
-    labelInput.setAttribute("for","category");
-    labelInput.innerHTML = "Let's try \"Fantasy\" ";
-    form.append(labelInput);
+labelInput.setAttribute("id", "labelInput");
+labelInput.setAttribute("for", "category");
+labelInput.innerHTML = 'Let\'s try "Fantasy" ';
+form.append(labelInput);
 
 let inputForm = document.createElement("input");
-    inputForm.setAttribute("id", "category");
-    inputForm.setAttribute("type", "text");
-    inputForm.setAttribute("placeholder", "Write a Category...");
-    inputForm.setAttribute("required", true);
-    inputForm.setAttribute("autocomplete", "off");
-    form.append(inputForm);
+inputForm.setAttribute("id", "category");
+inputForm.setAttribute("type", "text");
+inputForm.setAttribute("placeholder", "Write a Category...");
+inputForm.setAttribute("required", true);
+inputForm.setAttribute("autocomplete", "off");
+form.append(inputForm);
 
 let inputBtn = document.createElement("button");
-    inputBtn.setAttribute("type","submit");
-    inputBtn.setAttribute("id","inputBtn");
-    inputBtn.setAttribute("value","");
-    inputBtn.innerHTML = `<i class="fa-solid fa-magnifying-glass"></i>`;
-    form.append(inputBtn);
+inputBtn.setAttribute("type", "submit");
+inputBtn.setAttribute("id", "inputBtn");
+inputBtn.setAttribute("value", "");
+inputBtn.innerHTML = `<i class="fa-solid fa-magnifying-glass"></i>`;
+form.append(inputBtn);
 
+searchBox.append(form);
 
-    searchBox.append(form);
+inputForm.addEventListener("focusin", () => {
+  labelInput.style.visibility = "visible";
+});
 
-inputForm.addEventListener(
-    "focusin", () => { labelInput.style.visibility = "visible" });
-
-inputForm.addEventListener(
-    "focusout", () => { labelInput.style.visibility = "hidden" });
+inputForm.addEventListener("focusout", () => {
+  labelInput.style.visibility = "hidden";
+});
 
 // CODE --> BOOKS RESULTS //
 
@@ -71,62 +71,64 @@ let back = document.createElement("button");
 let noFound = document.createElement("h3");
 noFound.classList.add("notFound");
 
-function lowerString(string){
-    return string.toLowerCase();
-};
+function lowerString(string) {
+  return string.toLowerCase();
+}
 
-async function jsonFetch(url){
-    const response = await fetch(url);
-    return await response.json();
-};
+async function jsonFetch(url) {
+  const response = await fetch(url);
+  return await response.json();
+}
 
-function showBooks(){
+function showBooks() {
+  searchBox.style.display = "none";
 
-    searchBox.style.display = "none";
+  back.innerHTML = `<i class="fa-solid fa-angles-left"></i>`;
+  back.classList.add("back");
 
-    back.innerHTML = `<i class="fa-solid fa-angles-left"></i>`;
-    back.classList.add("back");
+  let category = lowerString(inputForm.value);
 
-    let category = lowerString(inputForm.value);
-
-    jsonFetch(`https://openlibrary.org/subjects/${category}.json`)
-
+  jsonFetch(`https://openlibrary.org/subjects/${category}.json`)
     /*fetch(`https://openlibrary.org/subjects/${category}.json`)
     .then(response => response.json())*/
 
-    .then(data => {
-        console.log(data)
-        let books = "";
-        let Data = data.works
-        if(Data.length > 0){
-         for(let book of Data){
-            result.style.display = "flex";
+    .then((data) => {
+      console.log(data);
+      let books = "";
+      let Data = data.works;
+      if (Data.length > 0) {
+        for (let book of Data) {
+          result.style.display = "flex";
           books += `
             <div class="singleBook" data-book="${book.key}">
             <h2 id="title">${book.title}</h2>
-            <img src=${"https://covers.openlibrary.org/b/id/" + book.cover_id + "-M.jpg"} alt="${book.title}">
+            <img src=${
+              "https://covers.openlibrary.org/b/id/" + book.cover_id + "-M.jpg"
+            } alt="${book.title}">
             <h5>${book.authors[0].name}</h5>
             <p id="description"></p>
             </div>
-            `
-            result.innerHTML = books
-            result.append(back);
-
+            `;
+          result.innerHTML = books;
+          result.append(back);
         }
-    } else {
+      } else {
         result.innerHTML = "";
         result.append(noFound, back);
         result.style.display = "block";
         noFound.style.display = "block";
-        noFound.innerHTML = "Ops, the category of books you are looking for doesn't exist." + "<br/>" + " Please go back to the \"Search Box\" and try other category."
-    }
-    })
-};
+        noFound.innerHTML =
+          "Ops, the category of books you are looking for doesn't exist." +
+          "<br/>" +
+          ' Please go back to the "Search Box" and try other category.';
+      }
+    });
+}
 
 back.addEventListener("click", () => {
-    result.style.display = "none";
-    searchBox.style.display = "flex";
-    noFound.style.display = "none";
+  result.style.display = "none";
+  searchBox.style.display = "flex";
+  noFound.style.display = "none";
 });
 
 // CODE --> CHOOSEN BOOK //
@@ -135,87 +137,86 @@ const bookContainer = document.getElementById("bookContainer");
 const modal = document.getElementById("modal");
 
 let box1 = document.createElement("div");
-    let imageBook = document.createElement("img");
-    let authorBook = document.createElement("h5");
-    box1.append(imageBook, authorBook);
+let imageBook = document.createElement("img");
+let authorBook = document.createElement("h5");
+box1.append(imageBook, authorBook);
 
 let box2 = document.createElement("div");
-    let titleBook = document.createElement("h2");
-    let infoBook = document.createElement("p");
-    box2.append(titleBook, infoBook);
+let titleBook = document.createElement("h2");
+let infoBook = document.createElement("p");
+box2.append(titleBook, infoBook);
 
 let closeBnt = document.createElement("span");
-    box1.classList.add("detail-1");
-    box2.classList.add("detail-2");
-    closeBnt.classList.add("cross");
-    closeBnt.innerHTML = "&times;"
+box1.classList.add("detail-1");
+box2.classList.add("detail-2");
+closeBnt.classList.add("cross");
+closeBnt.innerHTML = "&times;";
 
-
-    modal.append(box1, box2, closeBnt);
+modal.append(box1, box2, closeBnt);
 
 result.addEventListener("click", showDetails);
 
-function showDetails(e){
-    let choosen = e.target.closest("div");
-    if(choosen.className != "singleBook") return
-    chooseBook()
+function showDetails(e) {
+  let choosen = e.target.closest("div");
+  if (choosen.className != "singleBook") return;
+  chooseBook();
 
-    modal.classList.add("no-overscroll");
+  modal.classList.add("no-overscroll");
 
-    let keyBook = choosen.dataset.book;
+  let keyBook = choosen.dataset.book;
 
-    jsonFetch(`https://openlibrary.org${keyBook}.json`)
+  jsonFetch(`https://openlibrary.org${keyBook}.json`)
     /*fetch(`https://openlibrary.org${keyBook}.json`)
     .then(response => response.json())*/
-    .then(data => {
-        titleBook.innerHTML = data.title;
+    .then((data) => {
+      titleBook.innerHTML = data.title;
 
-        if(!(data.covers)){
-            imageBook.src = "";
-            imageBook.alt = "Image not Found :("
-        } else {
-        imageBook.src = `https://covers.openlibrary.org/b/id/${data.covers[0]}-M.jpg`
-        }
+      if (!data.covers) {
+        imageBook.src = "";
+        imageBook.alt = "Image not Found :(";
+      } else {
+        imageBook.src = `https://covers.openlibrary.org/b/id/${data.covers[0]}-M.jpg`;
+      }
 
-        jsonFetch(`https://openlibrary.org${data.authors[0].author.key}.json`)
+      jsonFetch(`https://openlibrary.org${data.authors[0].author.key}.json`)
         /*fetch(`https://openlibrary.org${data.authors[0].author.key}.json`)
             .then(response => response.json())*/
-            .then(data => {
-                authorBook.innerHTML = data.name
-        })
+        .then((data) => {
+          authorBook.innerHTML = data.name;
+        });
 
-        if( !(data.description) ){
-            infoBook.innerHTML = "Description not found :( "
-        } else if (data.description.value === undefined){
-            infoBook.innerHTML = data.description
-        } else {
-            infoBook.innerHTML = data.description.value
-        };
-    })
-};
+      if (!data.description) {
+        infoBook.innerHTML = "Description not found :( ";
+      } else if (data.description.value === undefined) {
+        infoBook.innerHTML = data.description;
+      } else {
+        infoBook.innerHTML = data.description.value;
+      }
+    });
+}
 
-function chooseBook(){
-    if(bookContainer.hasAttribute("hidden")){
-        bookContainer.removeAttribute("hidden");
-    }
-};
+function chooseBook() {
+  if (bookContainer.hasAttribute("hidden")) {
+    bookContainer.removeAttribute("hidden");
+  }
+}
 
 closeBnt.addEventListener("click", () => {
-    if(!(bookContainer.hasAttribute("hidden"))){
-        bookContainer.setAttribute("hidden", true)
-    } else {
-        bookContainer.removeAttribute("hidden");
-    }
+  if (!bookContainer.hasAttribute("hidden")) {
+    bookContainer.setAttribute("hidden", true);
+  } else {
+    bookContainer.removeAttribute("hidden");
+  }
 });
 
 document.addEventListener("keydown", (e) => {
-    if(e.key === "Escape"){
-        bookContainer.setAttribute("hidden", true)
-    }
-})
+  if (e.key === "Escape") {
+    bookContainer.setAttribute("hidden", true);
+  }
+});
 
-bookContainer.addEventListener("click", e => {
-    if(e.target == bookContainer){
-        bookContainer.setAttribute("hidden", true)
-    }
-})
+bookContainer.addEventListener("click", (e) => {
+  if (e.target == bookContainer) {
+    bookContainer.setAttribute("hidden", true);
+  }
+});
